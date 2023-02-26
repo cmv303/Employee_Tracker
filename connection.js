a8456087be615878c4532b6070984ed2d0f4f31a
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-02-26 08:50:51 -0500

    still trying to figure out the queries. Cannot get them to work

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 //This file contains database connection setup code
 
 //dependencies required
+require ('dotenv').config();
 const mysql = require ('mysql2');
-const dotenv = require ('dotenv');
-dotenv.config();
+
 
 //stores mysql connection in a variable and attaches a promise method to it, allowing asynchoronous functions to be used instead of callbacks from here on out
 const dbConnection = mysql.createPool ({
@@ -13,4 +13,4 @@ const dbConnection = mysql.createPool ({
     database: process.env.MYSQL_DATABASE
 });
 
-module.exports = dbConnection;
+module.exports = dbConnection.promise();
--- server.js
+++ server.js
@@ -1,153 +1,37 @@
 //This file is the entry point of the application. 
 
-
 //dependencies required
-const connection = require("./connection");
-
-
+// const connection = require("./connection");
 const inquirer = require("inquirer");
 
-
-
-
-const Questionnaire = {
-   addToQuestions: [
-      {
+inquirer
+  .prompt([
+    {
         type: "list",
-        message: "What would you like to add?",
-        name: "primary add question",
-        choices: [
-          "Add Department",
-          "Add a Role",
-          "Add an Employee",
-          "Update an Employee Role",
-          "Quit",
-        ],
-        default: "Add Department",
-      },
-      {
-        type: "input",
-        message: "What is the name of the department you would like to add?",
-        name: "new department",
-        default: "Customer Service",
-      },
-      {
-        type: "list",
-        message: "What would you like to add?",
-        name: "primary add question",
-        choices: [
-            "Add Department",
-            "Add a Role",
-            "Add an Employee",
-            "Quit"
+        message: "What would you like to do?",
+        name: "select",
+        choice: ["View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update employee"
             ],
-        default: "Add an Employee",
-      },
-      {
-        type: "input",
-        message: "What is the name of the Employee you would like to add?",
-        name: "employee name",
-        default: "Evelyn Mitchell",
-      },
-      {
-        type: "list",
-        message: "What would you like to add?",
-        name: "primary add question",
-        choices: [
-          "Add Department",
-          "Add a Role",
-          "Add an Employee",
-          "Update an Employee Role",
-          "Quit",
-        ],
-        default: "Add a Role",
-      },
-      {
-        type: "input",
-        message: "What is the name of the role you would like to add?",
-        name: "new role",
-        default: "Team Lead",
-      },
-    ],
+        default:"View all employees"
+    },
+  ])
+  .then((answer) => {
+    console.info("select choice:", select)
+  });
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
 
-viewAllQuestions: [
-      {
-        type: "list",
-        message: "What would you like to view?",
-        name: "primary view question",
-        choices: [
-          "View all Departments",
-          "View all Roles",
-          "View all Employees",
-          "Quit",
-        ],
-        default: "View all Employees",
-      },
-    ],
-
-   updateQuestions: [
-      {
-        type: "list",
-        message: "What would you like to update?",
-        name: "update",
-        choices: ["Update Employee Role", "Update Salary"],
-        default: "Update Salary",
-      },
-    ],
-
-  async askAddQuestions() {
-    const answers = await inquirer.prompt(this.addToQuestions);
-    console.log(answers);
-  },
-
-  async askViewQuestions() {
-    const answers = await inquirer.prompt(this.viewAllQuestions);
-    console.log(answers);
-  },
-
-  async askUpdateQuestions() {
-    const answers = await inquirer.prompt(this.updateQuestions);
-    console.log(answers);
-  },
-};
-
-const questionnaire = Object.create(Questionnaire);
-
-async function run() {
-    while (true) {
-      console.log("We got here!");
-        const { action } = await inquirer.prompt([
-            {
-                type: "list",
-                message: "What would you like to do?",
-                name: "action",
-                choices: [
-                    "Add",
-                    "View",
-                    "Update",
-                    "Quit",
-                ],
-                default: "Add",
-            }
-        ]);
-        
-
-        if (action === "Add") {
-            await questionnaire.askAddQuestions();
-        } else if (action === "View") {
-            await questionnaire.askViewQuestions();
-        } else if (action === "Update") {
-            await questionnaire.askUpdateQuestions();
-        } else {
-            break;
-        }
-    }
-}
-
-
-
-run();
-
-module.exports = connection;
+// module.exports = connection;
 
 
