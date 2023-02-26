bfa7a867f4d5557a76fe5f9286597c706a94339e
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-02-26 16:05:20 -0500

    started working on queries, but I'm having trouble making a connection

--- server.js
+++ server.js
@@ -1,37 +1,98 @@
 //This file is the entry point of the application. 
 
 //dependencies required
-// const connection = require("./connection");
+const dbConnection = require("./connection");
 const inquirer = require("inquirer");
+const consoleTable = require("console.table"); //!where should I call
 
-inquirer
-  .prompt([
-    {
+
+class Questionnaire {
+    async start() {
+    const { select } = await inquirer.prompt([
+        {
         type: "list",
         message: "What would you like to do?",
         name: "select",
-        choice: ["View all departments",
+        choices: [
+            "View all departments",
             "View all roles",
             "View all employees",
             "Add a department",
             "Add a role",
             "Add an employee",
-            "Update employee"
+            "Update employee",
+            "Quit"
             ],
         default:"View all employees"
-    },
-  ])
-  .then((answer) => {
-    console.info("select choice:", select)
-  });
-//   .catch((error) => {
-//     if (error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else went wrong
-//     }
-//   });
+        },
+    ]);
 
-// module.exports = connection;
+switch (select) {
+    case 'View all departments':
+        await this.viewAllDepartments();
+        break;
+    case 'View all roles':
+        await this.viewAllRoles();
+        break;
+    case 'View all employees':
+        await this.viewAllEmployees();
+        break;
+    case 'Add a department':
+        await this.addADepartment();
+        break;
+    case 'Add a role':
+        await this.addARole();
+        break;
+    case 'Add an employee':
+        await this.addAnEmployee();
+        break;
+   case 'Update an employee':
+        await this.updateAnEmployee();
+        break;
+    case 'Quit':
+        console.log('See ya!')
+        return;
+    }
+}
+
+async viewAllDepartments() {
+        try{
+            const connection = await dbConnection;
+            const [rows] = await connection.query('SELECT * FROM department');
+            console.table('All records in department table:', rows);
+            this.start();
+        } catch (error) {
+            console.error('Error executing query:', error)
+        }
+    }
+
+
+async viewAllRoles() {
+    try{
+        const connection = await dbConnection;
+        const [rows] = await connection.query('SELECT * FROM roles');
+        console.table('All records in roles table:', rows);
+        this.start();
+    } catch (error) {
+        console.error('Error executing query:', error);
+    }
+}
+
+async viewAllEmployees() {
+    try{
+        const connection = await dbConnection;
+        const [rows] = await connection.query('SELECT * FROM employee');
+        console.table('All records in employee table:', rows);
+        this.start();
+    } catch (error) {
+        console.error('Error executing query:', error);
+    }
+}
+
+};
+
+const questionnaire = new Questionnaire();
+questionnaire.start();
+
 
 
