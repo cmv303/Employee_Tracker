e86eb62f8e63ce8e15d50e04c248cb9b58cec234
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-02-28 22:54:41 -0500

    got everything to work except for the add role, which is still buggy

--- server.js
+++ server.js
@@ -32,12 +32,7 @@ const addMenu = [
     type: "list",
     message: "What would you like to add?",
     name: "add",
-    choices: [
-      "Add a department",
-      "Add a roles",
-      "Add an employee",
-      "Main menu",
-    ],
+    choices: ["Add a department", "Add a role", "Add an employee", "Main menu"],
   },
 ];
 
@@ -53,126 +48,186 @@ const updateMenu = [
 
 // Define the start function, which is the main entry point of the application
 async function start() {
-    const { main } = await inquirer.prompt(mainMenu);
+  const { main } = await inquirer.prompt(mainMenu);
 
-    if (main === "View") {
-        const { view } = await inquirer.prompt(viewMenu);
+  if (main === "View") {
+    const { view } = await inquirer.prompt(viewMenu);
 
-        if (view === "View departments") {
-          try {
-            const rows = await dbConnection.query("SELECT * FROM department");
-            console.log("rows", rows);
-            console.table("All records in department table:", rows[0]);
-          } catch (error) {
-            console.error("Error executing query:", error);
-          }
-          start.bind(this)();
-        } else if (view === "View roles") {
-          try {
-            const rows = await dbConnection.query("SELECT role.id, role.title, role.salary, department.dept_name FROM role JOIN department ON role.department_id = department.id");
-            console.log("ROWSSSSS", rows)
-            console.table("All records in role table:", rows [0]);
-          } catch (error) {
-            console.error("Error executing query:", error);
-          }
-          start.bind(this)();
-        } else if (view === "View employees") {
-          try {
-            const rows = await dbConnection.query("SELECT employee.first_name, employee.last_name, role.title, department.dept_name AS department, role.salary, CONCAT (manager.first_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id");
-            console.log("Employee????", rows)
-            console.table("All records in employee table:", rows [0]);
-          } catch (error) {
-            console.error("Error executing query:", error);
-          }
-          start.bind(this)();
-        } else if (view === "Main menu") {
-          start();
-        }
-      
-    } else if (main === "Add") {
-        const { add } = await inquirer.prompt(addMenu);
-
-        if (add === "Add a department") {
-            try {
-                //!Not pushing to db)
-                const { dept_name } = await inquirer.prompt({
-                    type: "input",
-                    name: "deptName",
-                    message:"What is the name of the department you would like to add?",
-                })
-                const result = await dbConnection.query("INSERT INTO department (dept_name) VALUES (?)", [dept_name]);
-                console.table("Department '${deptName}' added with ID ${result.id}", result);
-              } catch (error) {
-                console.error("Error executing query:", error);
-              }
-              start.bind(this)();
-          //call function
-        } else if (add === "Add a role") {
-          //call function
-        } else if (add === "Add an employee") {
-            try {
-                //! have to reformat once I get the above ones to work
-                const rows = await dbConnection.query("INSERT INTO employee VALUES (first_name,last_name)");
-                console.table("What is the employee name you would like to add? Format required: first name, last name:", rows);
-              } catch (error) {
-                console.error("Error executing query:", error);
-              }
-              start.bind(this)();
-          //call function
-        } else if (add === "Main menu") {
-          start(); //break out of while loop and return to main menu
-        }
-    } else if (main === "Update") {
-        const { update } = await inquirer.prompt(updateMenu);
-
-        if (update === "Update an employee") {
-          //call function
-        } else if (update === "Main menu") {
-          start(); //break out of while loop and return to main menu
-        }
-    } else if (main === "Quit") {
-      //exit the node.js process entirely
-      process.exit(0);
+    if (view === "View departments") {
+      try {
+        const rows = await dbConnection.query("SELECT * FROM department");
+        console.log("rows", rows);
+        console.table("All records in department table:", rows[0]);
+      } catch (error) {
+        console.error("Error executing query:", error);
+      }
+      start();
+    } else if (view === "View roles") {
+      try {
+        const rows = await dbConnection.query(
+          "SELECT role.id, role.title, role.salary, department.dept_name FROM role JOIN department ON role.department_id = department.id"
+        );
+        console.log("ROWSSSSS", rows);
+        console.table("All records in role table:", rows[0]);
+      } catch (error) {
+        console.error("Error executing query:", error);
+      }
+      start();
+    } else if (view === "View employees") {
+      try {
+        const rows = await dbConnection.query(
+          "SELECT employee.first_name, employee.last_name, role.title, department.dept_name AS department, role.salary, CONCAT (manager.first_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id"
+        );
+        console.log("Employee????", rows);
+        console.table("All records in employee table:", rows[0]);
+      } catch (error) {
+        console.error("Error executing query:", error);
+      }
+      start();
+    } else if (view === "Main menu") {
+      start();
     }
+  } else if (main === "Add") {
+    const { add } = await inquirer.prompt(addMenu);
+    if (add === "Add a department") {
+      try {
+        const { deptName } = await inquirer.prompt({
+          type: "input",
+          name: "deptName",
+          message: "What is the name of the department you would like to add?",
+        });
+        console.log("Department Name", deptName);
+        const result = await dbConnection.query(
+          `INSERT INTO department (dept_name) VALUES (?)`,
+          [deptName]
+        );
+        console.table(
+          `department ${deptName} added with ID ${result.id}`,
+          result[0]
+        );
+      } catch (error) {
+        console.error("Error executing query:", error);
+      }
+      start();
+    } else if (add === "Add a role") {
+      try {
+        const { roleName, roleSalary } = await inquirer.prompt([
+          {
+            type: "input",
+            name: "roleName",
+            message: "What is the name of the role you would like to add?",
+          },
+          {
+            type: "input",
+            name: "roleSalary",
+            message: "What is the salary of the role you would like to add?",
+          }
+        ]);
+        console.log("Role Name, role salary", roleName, roleSalary);
+        const rows = await dbConnection.query(
+          `INSERT INTO role (title, salary) VALUES (?, ?)`,
+          [roleName, roleSalary]
+        );
+        console.table(
+          `role added!`,
+          { role: roleName, salary: roleSalary }, rows[0]
+        );
+      } catch (error) {
+        console.error("Error executing query:", error);
+      }
+      start();
+    } else if (add === "Add an employee") {
+      try {
+        const { firstName, lastName, roleId, managerId } =
+          await inquirer.prompt([
+            {
+              type: "input",
+              name: "firstName",
+              message: "What is the employee's first name?",
+            },
+            {
+              type: "input",
+              name: "lastName",
+              message: "What is the employee's last name?",
+            },
+            {
+              type: "input",
+              name: "roleId",
+              message: "What is the employee's role ID?",
+            },
+            {
+              type: "input",
+              name: "managerId",
+              message: "What is the employee's manager ID?",
+            },
+          ]);
+        const result = await dbConnection.query(
+          `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+          [firstName, lastName, roleId, managerId]
+        );
+        console.table(
+          `Employee ${firstName} ${lastName} added with ID ${result.insertId}`
+        );
+      } catch (error) {
+        console.error("Error executing query:", error);
+      }
+      start();
+      //call function
+    } else if (add === "Main menu") {
+      start(); //break out of while loop and return to main menu
+    }
+  } else if (main === "Update") {
+    const { update } = await inquirer.prompt(updateMenu);
+
+    if (update === "Update an employee") {
+      try {
+        const { employeeId } = await inquirer.prompt({
+          type: 'input',
+          name: 'employeeId',
+          message: 'Enter the ID of the employee you want to update:'
+        });
+    
+        // Retrieve the existing employee information from the database
+        const [rows] = await dbConnection.query('SELECT * FROM employee WHERE id = ?', [employeeId]);
+    
+        if (rows.length === 0) {
+          console.log(`No employee found with ID ${employeeId}.`);
+          return;
+        }
+        const { first_name, last_name } = rows[0];
+    
+        // Prompt the user to enter the new information for the employee
+        const { newFirstName, newLastName } = await inquirer.prompt([
+          {
+            type: 'input',
+            name: 'newFirstName',
+            message: `Enter the employee's new first name (${first_name}):`,
+          },
+          {
+            type: 'input',
+            name: 'newLastName',
+            message: `Enter the employee's new last name (${last_name}):`,
+          }
+        ]);
+    
+        // Update the employee record in the database with the new information
+        const [result] = await dbConnection.query(
+          'UPDATE employee SET first_name = ?, last_name = ? WHERE id = ?',
+          [newFirstName, newLastName, employeeId]
+        );
+        console.table(`Employee updated!`, result[0]);
+      } catch (error) {
+        console.error("Error executing query:", error);
+      }
+      start();
+    } else if (update === "Main menu") {
+      start(); //break out of while loop and return to main menu
+    }
+  } else if (main === "Quit") {
+    //exit the node.js process entirely
+    process.exit(0);
+  }
 }
 
 //call the start function
 start();
-
-
-// //! how do I connect this to query.sql?' NOT SURE ABOUT MY QUERIES HERE
-// async addARole() {
-//     try{
-//         const connection = await dbConnection;
-//         const [rows] = await connection.query('INSERT INTO role (title, salary, department_id) VALUES (?, salary, department_id)');
-//         console.table('role added!', rows);
-//         this.start();
-//     } catch (error) {
-//         console.error('Error executing query:', error)
-//     }
-// }
-// //! how do I connect this to query.sql? NOT SURE ABOUT MY QUERIES HERE
-// async addAnEmployee() {
-//     try{
-//         const connection = await dbConnection;
-//         const [rows] = await connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, role_id, manager_id');
-//         console.table('employee added!', rows);
-//         this.start();
-//     } catch (error) {
-//         console.error('Error executing query:', error)
-//     }
-// }
-// //! how do I connect this to query.sql? NOT SURE ABOUT MY QUERIES HERE
-// async updateAnEmployee() {
-//     try{
-//         const connection = await dbConnection;
-//         const [rows] = await connection.query('UPDATE employee SET role_id = new role_id WHERE = employee_id');
-//         console.table('employee updated!', rows);
-//         this.start();
-//     } catch (error) {
-//         console.error('Error executing query:', error)
-//     }
-// }
-
-// const questionnaire = new Questionnaire();
-// questionnaire.start();
